5022027a922fbab92e8dd5926ec20e757254f475
Daniel Marval <daniel.marval.93@gmail.com>
2022-11-27 18:58:45 +0100

    added information of services

--- src/components/HomepageLayout.js
+++ src/components/HomepageLayout.js
@@ -402,7 +402,7 @@ const HomepageLayout = () => (
         <Grid divided inverted stackable>
           <Grid.Row></Grid.Row>
           <Grid.Row>
-            <Grid.Column width={3}>
+            <Grid.Column width={6} centered >
               <Header inverted as="h4" content="Contacto" />
               <List link inverted>
                 <List.Item as="a"><FaRegEnvelope/> info@dvinemodels.com</List.Item>

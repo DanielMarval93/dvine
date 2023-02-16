0a0f61baedf5e2c3ac0511cd1a8995e72d8ebc2c
Daniel Marval <daniel.marval.93@gmail.com>
2023-02-16 18:05:14 +0100

    a cerca

--- src/components/Segments/AboutUs.js
+++ src/components/Segments/AboutUs.js
@@ -10,7 +10,7 @@ const AboutUs = () => {
           horizontal
           style={{ margin: "0em 0em 1em", textTransform: "uppercase" }}
         >
-          <p>A cerca de nosotros</p>
+          <p>Acerca de nosotros</p>
         </Divider>
         <Grid container centered stackable verticalAlign="middle">
         <Grid.Row>
